0f4e3b02cd4729b68c6fbaf876b317434d38c488
Michał Rosa <michal.dew@gmail.com>
2021-05-01 18:45:23 +0200

    Removed unnecessary css classes

--- src/components/CurrentRecipe/CurrentRecipe.js
+++ src/components/CurrentRecipe/CurrentRecipe.js
@@ -2,25 +2,30 @@ import { RecipeMacroSummary, Macro, MacroHeader, EnergyRatioWrapper } from './st
 
 const CurrentRecipe = () => {
     return (
-        <RecipeMacroSummary>
-            <Macro>
-            <MacroHeader class="summary__header">B</MacroHeader><span class="js--summary__macro--protein">0.00</span>
-          </Macro>
-          <Macro>
-            <MacroHeader class="summary__header">T</MacroHeader><span class="js--summary__macro--fat">0.00</span>
-          </Macro>
-          <Macro>
-            <MacroHeader class="summary__header">W</MacroHeader><span class="js--summary__macro--carbohydrates">0.00</span>
-          </Macro>
-          <EnergyRatioWrapper>
-            <div class="summary__energy-wrapper">
-              Kcal: <span class="js--summary__macro--energy">0.00</span>
-            </div>
-            <div class="summary__ratio-wrapper">
-              Stosunek ketogenny <span class="js--summary__macro--ratio">-- : 1</span>
-            </div>
-          </EnergyRatioWrapper>
-        </RecipeMacroSummary>
+        <>
+            <RecipeMacroSummary>
+                <Macro>
+                    <MacroHeader>B</MacroHeader>
+                    <span>0.00</span>
+                </Macro>
+                <Macro>
+                    <MacroHeader>T</MacroHeader>
+                    <span>0.00</span>
+                </Macro>
+                <Macro>
+                    <MacroHeader>W</MacroHeader>
+                    <span>0.00</span>
+                </Macro>
+                <EnergyRatioWrapper>
+                    <div>
+                        Kcal: <span>0.00</span>
+                    </div>
+                    <div>
+                        Stosunek ketogenny <span>-- : 1</span>
+                    </div>
+                </EnergyRatioWrapper>
+            </RecipeMacroSummary>
+        </>
     )
 };
 
