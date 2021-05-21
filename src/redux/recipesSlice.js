3b97a46e1d6671890ed8b4e5ee83b3ed61c96380
Michał Rosa <michal.dew@gmail.com>
2021-05-21 20:43:28 +0200

    Added updating recipe macro

--- src/redux/recipesSlice.js
+++ src/redux/recipesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { roundNumberTo2DecimalPlaces } from '../utils/mathUtils';
 
 export const emptyRecipe = {
     name: '',
@@ -10,6 +11,29 @@ export const emptyRecipe = {
     energy: '0.00',
     ratio: "-- : 1"
 };
+
+const updateRecipeMacro = currentRecipe => {
+    let protein = 0;
+    let fat = 0;
+    let carbohydrates = 0;
+
+    currentRecipe.ingredientsList.forEach(ing => {
+        protein += roundNumberTo2DecimalPlaces(ing.ingredient.protein * ing.weight / 100);
+        fat += roundNumberTo2DecimalPlaces(ing.ingredient.fat * ing.weight / 100);
+        carbohydrates += roundNumberTo2DecimalPlaces(ing.ingredient.carbohydrates * ing.weight / 100);
+    });
+
+    currentRecipe.protein = protein.toFixed(2);
+    currentRecipe.fat = fat.toFixed(2);
+    currentRecipe.carbohydrates = carbohydrates.toFixed(2);
+    currentRecipe.energy = getEnergy(protein, fat, carbohydrates).toFixed(2);
+    const ratio = getRatio(protein, fat, carbohydrates);
+    currentRecipe.ratio = `${isNaN(ratio) ? '--' : ratio.toFixed(2)} : 1`;
+}
+
+const getEnergy = (protein, fat, carbohydrates) => (protein + carbohydrates) * 4 + fat * 9;
+
+const getRatio = (protein, fat, carbohydrates) => fat / (protein + carbohydrates);
 
 const slice = createSlice({
     name: 'ketokalkulator',
@@ -27,15 +51,21 @@ const slice = createSlice({
         deleteIngredientFromCurrentRecipe: ({ currentRecipe }, { payload }) => {
             var index = currentRecipe.ingredientsList.findIndex(ing => ing.ingredient.id === payload);
             currentRecipe.ingredientsList.splice(index, 1);
+
+            updateRecipeMacro(currentRecipe);
         },
         changeIngredientInCurrentRecipe: ({ currentRecipe }, { payload }) => {
             var index = currentRecipe.ingredientsList.findIndex(ing => ing.ingredient.id === payload.oldIngredientId);
             currentRecipe.ingredientsList[index].ingredient = payload.ingredient;
+
+            updateRecipeMacro(currentRecipe);
         },
         changeIngredientWeightInCurrentRecipe: ({ currentRecipe }, { payload }) => {
             var index = currentRecipe.ingredientsList.findIndex(ing => ing.ingredient.id === payload.ingredientId);
             const weight = payload.newWeight < 0 ? 0 : payload.newWeight;
             currentRecipe.ingredientsList[index].weight = weight;
+
+            updateRecipeMacro(currentRecipe);
         },
         addRecipe: ({ recipeList }, { payload }) => {
             recipeList.push(payload);
