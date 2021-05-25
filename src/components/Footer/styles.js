6f1f158d8ac2d507afd205007eb3cf08b6b142c0
Michał Rosa <michal.dew@gmail.com>
2021-05-25 23:17:03 +0200

    Added scroll to top button

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,5 +4,5 @@ export const FooterWrapper = styled.footer`
     background: ${({ theme }) => theme.footer.background};
     color: ${({ theme }) => theme.footer.color};
     font-size: 13px;
-    padding: 5px 10px;
+    padding: 5px 10px 30px;
 `;
---
+++ src/components/common/ScrollTopArrow/index.js
@@ -0,0 +1,2 @@
+import ScrollTopArrow from './ScrollTopArrow';
+export default ScrollTopArrow;
