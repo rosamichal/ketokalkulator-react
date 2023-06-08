a9d915c62e2e4fccc2976e776e962243563e462d
Michał Rosa <michal.rosa@action.pl>
2023-06-08 23:28:02 +0200

    fix

--- src/redux/saga/ingredientSaga.js
+++ src/redux/saga/ingredientSaga.js
@@ -1,16 +1,16 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
-import { getIngredients, setIngredients } from '../ingredientsSlice';
-import { persistKeys, writeData } from '../../utils/persistenceUtils';
-import { getIngredientsFromApi } from '../../api/fooddb-node';
+import { takeEvery, put, call } from "redux-saga/effects";
+import { getIngredients, setIngredients } from "../ingredientsSlice";
+import { persistKeys, writeData } from "../../utils/persistenceUtils";
+import { getIngredientsFromApi } from "../../api/fooddb-node";
 
 function* getIngredientHandler() {
     try {
         let ingredients = yield call(getIngredientsFromApi);
         ingredients = ingredients.sort((a, b) => a.name.localeCompare(b.name));
-        //yield put(setIngredients(ingredients));
+        yield put(setIngredients(ingredients));
         yield call(writeData, persistKeys.INGREDIENT_LIST, ingredients);
     } catch (error) {
-        yield call(console.error, `Something bad happened! ${error}`)
+        yield call(console.error, `Something bad happened! ${error}`);
     }
 }
 
